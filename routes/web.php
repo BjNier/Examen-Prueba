2076475043a2395115335ad609f8ea465a9c1dc1
BjNier <benjaminjmr703@gmail.com>
2024-10-18 18:06:55 -0500

    Envio del Primer Avance del examen

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+$url = $_SERVER['REQUEST_URI'];
+echo $url;
+if($url == "/examen/public/"){
+    require_once "../views/index.php";
+}elseif($url == "/examen/public/ejercicio01/"){
+    require_once "../views/ejercicio01.php";
+}elseif($url == "/examen/public/ejercicio02/"){
+    require_once "../views/ejercicio02.php";
+}elseif($url == "/examen/public/ejercicio03/"){
+    require_once "../views/ejercicio03.php";
+}else{
+    echo 'pagina no encontrada' ;
+}
